7c76a522985c588fb44b6ba0288d39c0dee7d635
Sammeet Koli <sammeet29@gmail.com>
2026-01-10 15:10:04 -0800

    Wrap up iterators

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -15,7 +13,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(mut first) =>
+        {
+            first.make_ascii_uppercase();
+            first.to_string() + &input[1..].to_string()
+        }
     }
 }
 
@@ -24,7 +26,12 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut results = vec![];
+    for word in words
+    {
+        results.push(capitalize_first(word));
+    }
+    results
 }
 
 // Step 3.
@@ -32,7 +39,12 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let capitalized_words = capitalize_words_vector(words);
+    let mut concatenated_string = String::new();
+    for word in capitalized_words{
+        concatenated_string = concatenated_string + &word;
+    }
+    concatenated_string
 }
 
 #[cfg(test)]
